d500070ef391d351aecbd8f244c6368f582ff2e6
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-08-29 11:55:41 -0300

    chore: main functionaliy of adding and removing move event

--- js/slide.js
+++ js/slide.js
@@ -6,15 +6,28 @@ export default class Slide {
 
   onStart(event) {
     event.preventDefault();
-    console.log(this);
+    console.log('mousedown');
+    this.wrapper.addEventListener('mousemove', this.onMove);
+  }
+
+  onMove(event) {
+    console.log('moved');
+  }
+
+  onEnd(event) {
+    console.log('event finished');
+    this.wrapper.removeEventListener('mousemove', this.onMove);
   }
 
   addSlideEvents() {
     this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
   }
 
   bindEvents() {
     this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
   }
 
   init() {
