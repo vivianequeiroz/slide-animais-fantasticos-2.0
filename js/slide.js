56fadf65ed56be7c8e88dda83967ff506ffdbc21
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-08-29 11:49:59 -0300

    chore: add of mousedown event with bind to get the slide obj and not slide-wrapper

--- js/slide.js
+++ js/slide.js
@@ -3,4 +3,23 @@ export default class Slide {
     this.slide = document.querySelector(slide);
     this.wrapper = document.querySelector(wrapper);
   }
+
+  onStart(event) {
+    event.preventDefault();
+    console.log(this);
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', this.onStart);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
 }
