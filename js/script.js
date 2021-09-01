9a25aabd9536fffea5f07889267325ff5e403085
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-09-01 17:30:12 -0300

    chore: add of next and prev buttons to improve navigation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-import Slide from './slide.js';
+import { SlideNav } from './slide.js';
 
-const slide = new Slide('.slide', '.slide-wrapper');
+const slide = new SlideNav('.slide', '.slide-wrapper');
 slide.init();
+slide.addArrow('.prev', '.next');
 console.log(slide);
-
-slide.changeSlide(3);
-slide.activePrevSlide();
